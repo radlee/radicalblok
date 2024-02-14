62291f31488827b2c381d16237f3e3d31d7fcf6b
lolito <prof.lolito@gmail.com>
2024-02-14 18:35:22 +0200

    Commiting Hard

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -6,7 +6,7 @@ function Footer() {
     <div className="max-w-2xl mx-auto text-center">
 
 	<footer className="p-4 bg-white rounded-lg shadow items-center p-6">
-		<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 < span className="rad">rad</span><span className="blak">Blok by origin< span className="rad">2020</span> | All Rights Reserved</span>
+		<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2023 - {new Date().getFullYear()} < span className="rad"> | rad</span><span className="blak">Blok | All Rights Reserved</span>
     </span>
 	</footer>
 </div>
